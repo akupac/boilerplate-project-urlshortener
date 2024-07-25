1a21385d3c53d467b9e4732e612d6b7300ccd908
Alexandre A Kupac <akupac@gmail.com>
2024-07-25 16:13:17 +0200

    Versão que passou nos testes

--- index.js
+++ index.js
@@ -7,75 +7,103 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 let URLModel = require('./urlModel');
 
-
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
- 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/hello', function (req, res) {
+  res.json({
+    greeting: 'hello API'
+  });
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.get('/api/shorturl/:short_url', async function(req, res) {
-  let url = await URLModel.findOne({ short_url: req.params.short_url })
+app.get('/api/shorturl/:short_url', async function (req, res) {
+  let url = await URLModel.findOne({
+    short_url: req.params.short_url
+  })
   if (url) {
     return res.redirect(url.original_url);
   } else {
-    return res.json({ error: 'invalid url' });
+    return res.json({
+      error: 'invalid url'
+    });
   }
 });
 
-app.post('/api/shorturl', 
-  function(req, res, next) {
-    console.log("\n\nreq.body", req.body);
-    console.log("\n\nreq.params", req.params);
+app.post('/api/shorturl',
+  async function (req, res, next) {
 
-    try {
-      if (!req.body.url.startsWith('http://') && !req.body.url.startsWith('https://')) {
-        req.body.parsedUrl = new URL(`http://${req.body.url}`);
-      } else {
-        req.body.parsedUrl = new URL(req.body.url);
-      }
-      dns.lookup(req.body.parsedUrl.host, (err) => {
-        if (err) {
-          return res.json({ error: 'invalid url' });
+      try {
+        const existingUrl = await URLModel.findOne({
+          short_url: req.body.short_url
+        });
+
+        if (existingUrl) {
+          return res.json({
+            original_url: existingUrl.original_url,
+            short_url: existingUrl.short_url
+          });
         } else {
-          next();
-        }
-      });
-    } catch (error) {
-      return res.json({ error: 'invalid url' });
-    }
-  }, 
-  function(req, res) {
-  var urlObj = new URLModel({ 
-    original_url: req.body.url, 
-    short_url: Math.floor(Math.random() * 2001)
-  });
-  urlObj.save().then((err, data) => {
-    if (err) return console.error(err);
-    done(null, data);
-  })
-  return res.json({ 
-    original_url: urlObj.original_url, 
-    short_url: urlObj.short_url 
-  }); 
-});
 
-app.listen(port, function() {
+        }
+        if (!req.body.url.startsWith('http://') && !req.body.url.startsWith('https://')) {
+          req.body.parsedUrl = new URL(`http://${req.body.url}`);
+        } else {
+          req.body.parsedUrl = new URL(req.body.url);
+        }
+        dns.lookup(req.body.parsedUrl.hostname, (err) => {
+          if (err) {
+            return res.json({
+              error: 'invalid url'
+            });
+          } else {
+            next();
+          }
+        });
+      } catch (error) {
+        return res.json({
+          error: 'invalid url'
+        });
+      }
+    },
+    async function (req, res) {
+      let count = await URLModel.countDocuments();
+
+      try {
+        var urlObj = new URLModel({
+          original_url: req.body.url,
+          short_url: count += 2
+        });
+
+        await urlObj.save()
+        return res.json({
+          original_url: urlObj.original_url,
+          short_url: urlObj.short_url
+        });
+
+      } catch (error) {
+        return console.error(error)
+      }
+
+    });
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
-
--- urlModel.js
+++ urlModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const urlSchema = new mongoose.Schema({
     original_url: {type: String,required: true},
-    short_url: {type: String,required: true, unique: true},
+    short_url: {type: Number,required: true, unique: true},
       });
 
 module.exports = mongoose.model('URLModel', urlSchema);
